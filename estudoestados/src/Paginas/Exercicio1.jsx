f56d231bbcfa4cc44616349f388e00b45670f67b
giovana122 <giovana.domingos@etec.sp.gov.br>
2025-10-07 12:23:47 -0300

    07/10/25
    07/10/25

--- estudoestados/src/Paginas/Exercicio1.jsx
+++ estudoestados/src/Paginas/Exercicio1.jsx
@@ -1,26 +1,54 @@
-export default function Exercicio1()
-{
+import  { useState } from 'react';
+
+export default function Exercicio1() {
+    //variavel de estado para os campos do formulario
+    const [numero, setNumero] = useState(0);
+
+    //variavel de estado para o resultado
+    const [resultado, setResultado] = useState(0);
+
+    //função para calcular o quadrado ou o cubo
+    function calcular() {
+        let quadrado, cubo;
+
+        quadrado = Number(numero) * Number(numero);
+        cubo = Number(numero) * Number(numero) * Number(numero);
+
+        setResultado("Quadrado:" + quadrado + "-Cubo" + cubo);
+    }
+
     return (
         <div>
-            <h1> Exercicio 1</h1>
+            <h1> Exercicio 1 </h1>
 
             <div className="conteudo">
+
+
                 <h3>calculo do quadrado/cubo</h3>
                 <form>
                     <p>
-                        digite um número qualquer: <br/>
-                        <input type= "text"/>
+                        digite um número qualquer: <br />
+
+                        <input type="text" value={numero}
+                            onChange={(e) => setNumero(e.target.value)} />
 
                     </p>
-                    <p> 
-                        <input type="button" value= "Exercicio 1"/>
+                    <p>
+                    <input type="button" value="Exercicio 1" onClik={calcular} />
                     </p>
                     <p>
-                        <a href= "/">Voltar</a>
+                        <b>resultado</b>
+                        <br />
+                        numero é {numero} <br />
+                        resultado é {resultado}
+                    </p>
+
+                    <p>
+                        <a href="/">Voltar</a>
                     </p>
                 </form>
 
             </div>
         </div>
-    )
+    );
 }
--- estudoestados/src/Paginas/Exercicio2.jsx
+++ estudoestados/src/Paginas/Exercicio2.jsx
@@ -1,5 +1,25 @@
+import React, { useState } from 'react';
+
 export default function Exercicio2()
+
 {
+
+   //variavel de estados para os campos do formulario
+   const [numero, setNumero] = useState(0);
+
+   //variavel de estados para o resultado
+   const [resultado, setResultado] = useState(0);
+
+   //função para calcular 
+   function calcular() {
+       let quadrado, cubo;
+
+       quadrado = Number(numero) * Number(numero);
+       cubo = Number(numero) * Number(numero) * Number(numero);
+setResultado("quadrado:"+quadrado+"cubo:"+cubo);
+
+
+   }
     return (
         <div>
             <h1> Exercicio 2</h1>
